9da6545ea21ca50f9f0fa3f697b9edc9a5e5e4e3
Scottys Guest Laptop <armand.scotty@yahoo.com>
2023-05-20 12:05:58 -0400

    introduce Countdown class

---
+++ Countdown.py
@@ -0,0 +1,25 @@
+import pygame
+import time
+
+class Countdown:
+    STAGES = ['Rock', 'Paper', 'Scissors', 'Shoot!']
+    SECONDS_PER_STAGE = 0.75
+
+    def __init__(self, game_display):
+        self.current_stage = -1
+        self.game_display = game_display
+    
+    def start(self):
+        self.current_stage = 0
+        self.current_stage_time_seconds = 0
+
+    def update(self, game_time_seconds):
+        if self.current_stage == -1 or self.current_stage == len(Countdown.STAGES):
+            return
+
+        self.current_stage_time_seconds += game_time_seconds
+
+        if self.current_stage_time_seconds >= Countdown.SECONDS_PER_STAGE:
+            self.current_stage += 1
+            self.current_stage_time_seconds -= Countdown.SECONDS_PER_STAGE
+        
