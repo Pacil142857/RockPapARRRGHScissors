1a47ad83a3b8245c42fee763af7cfc8fa18ea7c1
Scottys Guest Laptop <armand.scotty@yahoo.com>
2023-05-20 15:33:35 -0400

    Add bounce effect to countdown text

--- Countdown.py
+++ Countdown.py
@@ -13,7 +13,8 @@ class Countdown:
     def start(self):
         self.current_stage = 0
         self.current_stage_time_seconds = 0
-    
+        self.bounce_factor = 1.0
+
     def stop(self):
         self.current_stage = -1
         self.current_stage_time_seconds = 0
@@ -30,8 +31,16 @@ class Countdown:
         if self.current_stage_time_seconds >= Countdown.SECONDS_PER_STAGE and self.current_stage < len(Countdown.STAGES) - 1:
             self.current_stage += 1
             self.current_stage_time_seconds -= Countdown.SECONDS_PER_STAGE
-         
+            self.bounce_factor = 1.5
+
         current_stage_surface, _ = self.font.render(Countdown.STAGES[self.current_stage], (0, 0, 0))
         current_stage_rect = current_stage_surface.get_rect(center=(self.game_display.get_width()//2, self.game_display.get_height()//2 - 50))
-        self.game_display.blit(current_stage_surface, current_stage_rect)
+                
+        # apply bounce effect
+        if self.bounce_factor > 1.0:
+            self.bounce_factor *= 0.95
+
+        current_stage_surface = pygame.transform.scale(current_stage_surface, (int(current_stage_surface.get_width() * self.bounce_factor), int(current_stage_surface.get_height() * self.bounce_factor)))
+        current_stage_rect = current_stage_surface.get_rect(center=(self.game_display.get_width()//2, self.game_display.get_height()//2 - 50))
         
+        self.game_display.blit(current_stage_surface, current_stage_rect)
--- Game.py
+++ Game.py
@@ -19,38 +19,40 @@ class Game:
         self.input_window_time_seconds = 0
         
     def update(self):
-        if self.game_running:
-            key_down_events = []
+        if not self.game_running:
+            return
+        
+        key_down_events = []
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.game_running = False
-                if event.type == pygame.KEYDOWN:
-                    key_down_events.append(event)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.game_running = False
+            if event.type == pygame.KEYDOWN:
+                key_down_events.append(event)
 
-            clock_elasped_time_second = self.clock.get_time() / 1000.0
+        clock_elasped_time_second = self.clock.get_time() / 1000.0
+        
+        self.game_display.fill((255, 255, 255)) #fill with white color
+
+        #Reminder: clock.get_time() returns the time since the last call to clock.tick() in milliseconds
+        self.countdown.update(clock_elasped_time_second)
+
+        if self.countdown.current_stage >= len(Countdown.STAGES) - 1:
+            if self.input_window_time_seconds < Game.INPUT_WINDOW_SECONDS:
+                self.input_window_time_seconds += clock_elasped_time_second
+
+                if not self.player1.isReady():
+                    self.player1.chooseAttack(key_down_events)
+                if not self.player2.isReady():
+                    self.player2.chooseAttack(key_down_events)
+                    
+            elif self.input_window_time_seconds >= Game.INPUT_WINDOW_SECONDS and self.player1.isReady() and self.player2.isReady():
+                self.player1.fight(self.player2)
+                self.input_window_time_seconds = 0
+                self.countdown.stop()
+            elif not self.player1.isReady() and not self.player2.isReady():
+                self.input_window_time_seconds = 0
+                self.countdown.stop()
             
-            self.game_display.fill((255, 255, 255)) #fill with white color
-
-            #Reminder: clock.get_time() returns the time since the last call to clock.tick() in milliseconds
-            self.countdown.update(clock_elasped_time_second)
-
-            if self.countdown.current_stage >= len(Countdown.STAGES) - 1:
-                if self.input_window_time_seconds < Game.INPUT_WINDOW_SECONDS:
-                    self.input_window_time_seconds += clock_elasped_time_second
-
-                    if not self.player1.isReady():
-                        self.player1.chooseAttack(key_down_events)
-                    if not self.player2.isReady():
-                        self.player2.chooseAttack(key_down_events)
-                        
-                elif self.input_window_time_seconds >= Game.INPUT_WINDOW_SECONDS and self.player1.isReady() and self.player2.isReady():
-                    self.player1.fight(self.player2)
-                    self.input_window_time_seconds = 0
-                    self.countdown.stop()
-                elif not self.player1.isReady() and not self.player2.isReady():
-                    self.input_window_time_seconds = 0
-                    self.countdown.stop()
-                
-            pygame.display.update()
-            self.clock.tick(60)
+        pygame.display.update()
+        self.clock.tick(60)
