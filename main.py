6622bc93992a747281338deae416c5b64edbfaca
Scottys Guest Laptop <armand.scotty@yahoo.com>
2023-05-20 11:36:23 -0400

    basic window in pygame in main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#create a starter project using pygame
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+game_display = pygame.display.set_mode((display_width, display_height))
+
+pygame.display.set_caption('Test')
+
+white = (255, 255, 255)
+clock = pygame.time.Clock()
+
+# define a game loop
+def game_loop():
+    game_running = True
+    
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_running = False
+        
+        game_display.fill(white)
+                
+        pygame.display.update()
+        
+        clock.tick(60)
+
+game_loop()
+
+pygame.quit()
